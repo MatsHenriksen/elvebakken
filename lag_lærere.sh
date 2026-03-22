1f066eec0302677c4e102f467cb7a9e46894ac95
Mats Henriksen <mats.henriksen.mmvii@gmail.com>
2026-03-22 20:41:36 +0100

    Fikset script

--- lag_lærere.sh
+++ lag_lærere.sh
@@ -5,7 +5,7 @@ lererfil="lærere.csv"
 tail -n +2 "$lererfil" | while IFS="," read -r klasse lerer fag; do
   #Lager lerere
   lerer=$(echo "$lerer" | tr -d '\r')
-  brukernavn=$(echo "$lerer" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+  brukernavn=$(echo "$lerer" | tr 'ø' 'o' | tr 'Ø' 'o' | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
   echo "Lager bruker med brukernavn: $brukernavn, klasse: $klasse, lerer: $lerer, fag: $fag"
   useradd -c "$lerer" "$brukernavn"
   echo "$brukernavn:Passord123" | sudo chpasswd
