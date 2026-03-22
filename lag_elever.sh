dad9656c9ba1cafbe7efbb07fec071dea11a58ef
Mats Henriksen <mats.henriksen.mmvii@gmail.com>
2026-03-22 16:30:28 +0100

    Endret stor ø til liten o

--- lag_elever.sh
+++ lag_elever.sh
@@ -6,7 +6,7 @@ fagfil="fag.csv"
 tail -n +2 "$elevfil" | while IFS="," read -r linje klasse elev fritak; do
   # Lager elev
   elev=$(echo "$elev" | tr -d '\r')   # fjerner \r
-  brukernavn=$(echo "$elev" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+  brukernavn=$(echo "$elev" | tr '[:upper:]' '[:lower:]' | tr 'Ø' 'o' | tr ' ' '_')
   echo "Lager bruker med brukernavn: $brukernavn, linje: $linje, klasse: $klasse, elev: $elev, fritak: $fritak" 
   useradd -c "$elev" "$brukernavn"
   echo "$brukernavn:Passord123" | sudo chpasswd   # fungerer på Ubuntu
